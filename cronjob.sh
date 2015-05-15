84fddbcac4f416f49e3ff65bf0133cb080f52f29
prashant7090 <prashant7090@gmail.com>
2015-05-15 09:46:18 +0530

    using rubymine insted of eclipse

--- cronjob.sh
+++ cronjob.sh
@@ -3,5 +3,5 @@
 echo "Good Morning $USER" &          #ampersand sign instruct that after executing current line rest of line should execute 
 /usr/bin/google-chrome-stable http://www.gmail.com http://www.github.com http://www.asana.com &
 /usr/bin/thunderbird &               #path for thunderbird (intrested application)
-/home/techbulls18/eclipse/eclipse &
+/home/techbulls18/software/RubyMine-7.1.1/bin/rubymine.sh &
 echo "Bye TC"
