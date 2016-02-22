2b62aa6412648d05f0181baf2e37edaf09426122
prashant7090 <prashant7090@gmail.com>
2016-02-22 18:11:06 +0530

    Open apps between 9 am & 11 am.

--- cronjob.sh
+++ cronjob.sh
@@ -1,7 +1,8 @@
-#!/bin/bash                          
-#invoke bash to interpret instruction 
-echo "Good Morning $USER" &          #ampersand sign instruct that after executing current line rest of line should execute 
-/usr/bin/google-chrome-stable http://www.gmail.com http://www.github.com http://www.asana.com &
-/usr/bin/thunderbird &               #path for thunderbird (intrested application)
-/home/techbulls18/software/RubyMine-7.1.1/bin/rubymine.sh &
-echo "Bye TC"
+if [[ `date +%H` -ge 9 && `date +%H` -lt 11 ]];then
+	/usr/bin/google-chrome-stable http://www.gmail.com http://www.github.com &
+	/usr/bin/thunderbird &               #path for thunderbird (intrested application)
+	/home/techbulls18/eclipse/eclipse
+elif [ `date +%H` -ge 19 ]
+then
+	echo "Go Home"
+fi
